18c0085f98c50e060b4629ffa1d46d3bd8d621e1
wangyong <843096638@qq.com>
2019-09-25 00:55:39 +0800

    no message

--- http-demo/src/main/java/cn/wy/httpdemo/HttpDemoApplication.java
+++ http-demo/src/main/java/cn/wy/httpdemo/HttpDemoApplication.java
@@ -2,6 +2,9 @@ package cn.wy.httpdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class HttpDemoApplication {
         SpringApplication.run(HttpDemoApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Do any additional configuration here
+        return builder.build();
+    }
 }
